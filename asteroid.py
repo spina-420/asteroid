99a553141e1400f7783dfce97df5dc3533339497
spina-420 <spencer.plaud@gmail.com>
2026-01-02 21:58:03 -0500

    Chapter 4 Lesson 1

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import CircleShape
+from constants import LINE_WIDTH
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+    def update(self, dt):
+        print(super().update(self.position + self.velocity * dt))
+    
+        return super().update(self.position + self.velocity * dt)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     
@@ -10,14 +13,41 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    asteroid_field = AsteroidField()
+    
+    player_ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+
+        updatable.update(dt)
+        
+        for _ in drawable:
+            _.draw(screen)
+        
+        
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
+        
+
 
 if __name__ == "__main__":
     main()
